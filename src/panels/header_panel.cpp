8249bcbb7ad3805cca2e3665fef8f14ec9414a5e
Daniel M. Sahu <danielmohansahu@gmail.com>
2022-08-29 14:22:28 -0400

    Small formatting tweaks for header.

--- src/panels/header_panel.cpp
+++ src/panels/header_panel.cpp
@@ -16,9 +16,9 @@ HeaderPanel::HeaderPanel()
 
 void HeaderPanel::render(const std::optional<ComputationalGraph>&)
 {
-  mvwaddstr(_window, 0, 0, "ros-curses: Command line introspection of the ROS computational graph.");
-  mvwprintw(_window, 1, 0, "  Status: %s", _status.c_str());
-  mvwprintw(_window, 2, 0, "  Active: %u", _active);
+  mvwaddstr(_window, 0, 1, "ros-curses: Command line introspection of the ROS computational graph.");
+  mvwprintw(_window, 1, 2, "Status: %s", _status.c_str());
+  mvwprintw(_window, 2, 2, "Active: %u", _active);
   touchline(_window, 0, 3);
 }
 
