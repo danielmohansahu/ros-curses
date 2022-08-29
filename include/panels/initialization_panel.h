07d5a33e65d059fe14ee0721ce19436de1c8b4a7
Daniel M. Sahu <danielmohansahu@gmail.com>
2022-08-29 15:58:44 -0400

    Added an initialization panel with some debugging info.

---
+++ include/panels/initialization_panel.h
@@ -0,0 +1,45 @@
+/*
+ *
+ *
+ * 
+ */
+
+#pragma once
+
+// ros_curses
+#include "panel_base.h"
+
+namespace ros_curses::panels
+{
+
+/* Panel containing header information.
+ */
+class InitializationPanel : public PanelBase
+{
+ private:
+ public:
+
+  /* Class constructor.
+   */
+  InitializationPanel() {};
+
+  /* Generate complete display from the given fully container ComputationalGraph.
+   */
+  void render(const std::optional<ComputationalGraph>&) override;
+
+  /* Handle 'up' keystroke.
+   */
+  void handle_key_up() override {};
+
+  /* Handle 'down' keystroke.
+   */
+  void handle_key_down() override {};
+
+  /* Handle 'enter' keystroke.
+   */
+  void handle_enter() override {};
+
+
+}; // class HeaderPanel
+
+} // namespace ros_curses::panels
---
+++ src/panels/initialization_panel.cpp
@@ -0,0 +1,38 @@
+/*
+ *
+ *
+ * 
+ */
+
+#include <cstdlib>
+
+// access to environment variables
+extern char **environ;
+
+// ros_curses
+#include "panels/initialization_panel.h"
+
+namespace ros_curses::panels
+{
+
+void InitializationPanel::render(const std::optional<ComputationalGraph>&)
+{
+  // let the user know we haven't received any information
+  print_line(2, " No ROS connection found; waiting...");
+
+  // try to help the user with potentially useful debugging info
+  print_line(4, " (maybe) useful information:");
+  size_t idx = 5;
+
+  // look for variables that might be helpful in debugging:
+  char **evs = environ;
+  for (; *evs; evs++)
+    if (const auto& str = std::string(*evs); str.find("ROS_") != std::string::npos)
+      print_line(idx++, "\t" + str);
+
+  // redraw border (it might've gotten messed up)
+  draw_border();
+}
+
+
+} // namespace ros_curses::panels
