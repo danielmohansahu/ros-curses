0e184fc2b103ec9d05b9a20b85eabbb754a11bb0
Daniel M. Sahu <danielmohansahu@gmail.com>
2022-09-01 18:31:48 -0400

    Added scrolling and selection to the topic info panel.

--- include/panels/topic_info_panel.h
+++ include/panels/topic_info_panel.h
@@ -18,6 +18,18 @@ class TopicInfoPanel : public PanelBase
 {
  private:
 
+   // amount to shift to select next node (expected -1,1, but forward compatible)
+  int _shift {0};
+
+  // scrolling calculation helper class
+  ScrollRegion _scroll;
+
+  // last index displayed in our visible region, to implement scrolling hysteresis
+  size_t _last_start_idx {0};
+
+  // currently highlighted information
+  std::optional<std::pair<std::string, std::string>> _sub_selection;
+
  public:
 
   /* Generate complete display from the given fully container ComputationalGraph.
@@ -26,11 +38,23 @@ class TopicInfoPanel : public PanelBase
 
   /* Handle 'up' keystroke.
    */
-  void handle_key_up() override {};
+  void handle_key_up() override { _shift = -1; };
 
   /* Handle 'down' keystroke.
    */
-  void handle_key_down() override {};
+  void handle_key_down() override { _shift = 1; };
+
+  /* Handle 'enter' keystroke.
+   */
+  virtual ActionPacket handle_enter() override;
+
+  /* Override base class method to reset our state variables.
+   */
+  void set_visible(const bool visible) override;
+
+  /* Override base class method to reset our state variables.
+   */
+  void move_and_resize(const size_t rows, const size_t cols, const size_t y, const size_t x);
 
 }; // class TopicInfoPanel
 
--- src/panels/topic_info_panel.cpp
+++ src/panels/topic_info_panel.cpp
@@ -23,17 +23,84 @@ ActionPacket TopicInfoPanel::render(const std::optional<ComputationalGraph>& gra
   // otherwise print some info about this topic
   const auto& topic = graph->topics().at(*_selection);
 
+  // get a list of all this topic's information as tuples
+  std::vector<std::pair<std::string,std::string>> fields;
+  for (const auto& pub : topic->publishers)
+    fields.emplace_back("publisher", pub->name);
+  for (const auto& sub : topic->subscribers)
+    fields.emplace_back("subscriber", sub->name);
+
   // header information
-  print_line(1, *_selection + ((topic->active) ? " (active)" : " (inactive)"));
-  print_line(2, "  Message Type: " + topic->type);
-  print_line(3, "  Publishers:   " + std::to_string(topic->publishers.size()));
-  print_line(4, "  Subscribers:  " + std::to_string(topic->subscribers.size()));
+  size_t current_idx = 1;
+  print_line(current_idx++, *_selection + ((topic->active) ? " (active)" : " (inactive)"));
+  print_line(current_idx++, "  Message Type: " + topic->type);
+  print_line(current_idx++, "  Publishers:   " + std::to_string(topic->publishers.size()));
+  print_line(current_idx++, "  Subscribers:  " + std::to_string(topic->subscribers.size()));
+  print_line(current_idx++, "");
+
+  // check if we can shift to a valid sub-selection
+  size_t highlighted_idx;
+  if (const auto idx = _scroll.shift(_sub_selection, fields, _shift); !idx)
+    return NULL_ACTION;
+  else
+  {
+    // update selections
+    highlighted_idx = *idx;
+    _sub_selection = fields[*idx];
+    _shift = 0;
+  }
+
+  // start of the scrollable region (global coordinates)
+  const size_t scroll_start_idx = current_idx;
+
+  // get the region bounds
+  const auto [begin, end] = _scroll.range_from_selection(fields.size(), _last_start_idx, highlighted_idx);
+  _last_start_idx = begin;
+
+  // iterate through visible section of items and print
+  for (size_t i = begin; i != end; ++i)
+    print_line(scroll_start_idx + i - begin,
+               fields[i].first + ": " + fields[i].second,
+               (i == highlighted_idx) ? A_STANDOUT : A_NORMAL);
+
+  // add a little blurb if we're scrolling
+  if (_scroll.scroll_required(fields.size()))
+    print_line_center(scroll_start_idx + end - begin, (end < fields.size() - 1) ? "-- more --" : "-- end --");
 
   // redraw border and header, in case it got borked
   draw_border();
   mvwaddnstr(_window, 0, 1, "ROS Topic Summary", _cols - 2 * BORDER);
 
   // indicate we want to display information about this topic
-  return {Action::SELECT_TOPIC, _selection};}
+  return NULL_ACTION;
+}
+
+ActionPacket TopicInfoPanel::handle_enter()
+{
+  // do nothing if nothing is highlighted
+  if (!_sub_selection)
+    return NULL_ACTION;
+
+  // switch to selected node
+  return {DISPLAY_NODE, _sub_selection->second};
+}
+
+void TopicInfoPanel::set_visible(const bool visible)
+{
+  // reset our selected index
+  _shift = 0;
+  _selection = std::nullopt;
+  _sub_selection = std::nullopt;
+
+  // call parent method
+  PanelBase::set_visible(visible);
+}
+
+void TopicInfoPanel::move_and_resize(const size_t rows, const size_t cols, const size_t y, const size_t x)
+{
+  // call parent method and update scroll size
+  PanelBase::move_and_resize(rows, cols, y, x);
+  _scroll.update_size(rows - BORDER * 2);
+}
 
 } // namespace ros_curses::panels
